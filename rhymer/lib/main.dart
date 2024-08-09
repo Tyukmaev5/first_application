125a8b6b7d886f4c909aaec71442c338e4d3aa23
tyukmaev <ityukmaev@sdvor.com>
2024-08-09 16:48:27 +0300

    Shared Prefences

--- rhymer/lib/main.dart
+++ rhymer/lib/main.dart
@@ -7,26 +7,34 @@ import 'package:rhymer/features/history/bloc/history_rhymes_bloc.dart';
 import 'package:rhymer/features/search/bloc/rhymes_list_bloc.dart';
 import 'package:rhymer/router/router.dart';
 import 'package:rhymer/ui/ui.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'cubit/cubit.dart';
 import 'repositories/history/history.dart';
+import 'repositories/settings/settings.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   final config = Configuration.local([HistoryRhymes.schema]);
   final realm = Realm(config);
-  runApp(
-    RhymerApp(
-      realm: realm,
-    ),
-  );
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  runApp(RhymerApp(
+    realm: realm,
+    preferences: prefs,
+  ));
 }
 
 class RhymerApp extends StatefulWidget {
-  const RhymerApp({super.key, required this.realm});
+  const RhymerApp({
+    super.key,
+    required this.realm,
+    required this.preferences,
+  });
 
   final Realm realm;
+  final SharedPreferences preferences;
 
   @override
   State<RhymerApp> createState() => _RhymerAppState();
@@ -38,6 +46,8 @@ class _RhymerAppState extends State<RhymerApp> {
   @override
   Widget build(BuildContext context) {
     final historyRepository = HistoryRepository(realm: widget.realm);
+    final settingsRepository =
+        SettingsRepository(preferences: widget.preferences);
 
     return MultiBlocProvider(
       providers: [
@@ -50,7 +60,10 @@ class _RhymerAppState extends State<RhymerApp> {
         BlocProvider(
             create: (context) =>
                 HistoryRhymesBloc(historyRepository: historyRepository)),
-        BlocProvider(create: (context) => ThemeCubit()),
+        BlocProvider(
+          create: (context) =>
+              ThemeCubit(settingsRepository: settingsRepository),
+        ),
       ],
       child: BlocBuilder<ThemeCubit, ThemeState>(
         builder: (context, state) {
